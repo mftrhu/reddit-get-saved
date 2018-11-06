21cfc5b7d29301aff12d44788792affceda0fafb
mftrhu <mftrhu+github@inventati.org>
2018-11-06 19:00:13 +0100

    Clean up the viewer code

--- viewer.py
+++ viewer.py
@@ -2,36 +2,25 @@
 import curses, textwrap, fileinput, json
 import webbrowser, subprocess, string, locale
 
-"""
-Bunch of todos:
-- [ ] Modify this to use Urwid/a decent widget library OR
-      - [ ] Create my own widget library
-            - TableWidget
-            - ScrollableText
-              - ScrollableANSIText
-                Will require a simple ANSI escape sequences parser and converter to
-                ncurses/Urwid colorpairs.
-            - MessageBox
-            - InputBox
-            - Widgets, once created, could register keybindings to the main window
-- [ ] Configurable keybindings/Vim keys
-- [ ] Filter by, freeform on body text, title, username, subreddit, tags
-- [ ] Filter by subreddit/user/time lapse/tags
-- [ ] Write back to file (s for save, maybe)
-- [ ] Archive item (shunt it to an archive file and remove it from the main one)
-- [ ] Tag item
-- [ ] Help window
-- [ ] d(ownload|ump): downloads linked image/whatnot *or* dumps text to selected file
-- [ ] Better markdown wrapper
-      - Chomp lines in code blocks to max length
-- [ ] Interface with an offline RES-alike (tagger, upvote counter) - maybe abook - to
-      provide information about the user who wrote the current comment
-      This could be bound to... [u], maybe, to show comments in collection, tag, upvotes,
-      and to allow one to add/change the usertag.
-- [ ] 
-"""
+def bind(value, start, end):
+    """
+    "Binds" a value between two endpoints.
+
+    Given three numbers `value`, `start` and `end`, returns `value` if
+    it is between `start` and `end`, `start` if `value` is lesser than
+    `start`, or `end` if it's greater than `end`.
+    """
+    return max(start, min(value, end))
 
 def count(string, char, skip_whitespace=False, return_offset=False):
+    """
+    Takes `string` and `char` as arguments, returning the number of
+    times that `char` occurs at the beginning of `string`, ignoring
+    any whitespace if `skip_whitespace` is true.
+
+    When `return_offset` is true, return both the count and the
+    offset in the string where the first non-`char` character occurs.
+    """
     i, result = 0, 0
     for i, c in enumerate(string):
         if skip_whitespace and c.isspace():
@@ -45,12 +34,21 @@ def count(string, char, skip_whitespace=False, return_offset=False):
     return result
 
 def md_wrap(text, width=70):
+    """
+    Given a Markdown-alike-formatted string `text`, it tries to word
+    wrap it to `width` (defaulting to 70 characters) while respecting
+    (indented) code blocks, blockquotes, and list.
+
+    Returns a list of lines.
+    """
     text = text.replace("&gt;", ">").replace("&lt;", "<").replace("&amp;", "&")
     lines = []
     for paragraph in text.split("\n\n"):
         if paragraph.startswith("    "):
+            # Code block
             lines.extend(paragraph.split("\n"))
         elif paragraph.startswith(">"):
+            # Blockquote
             items = paragraph.split("\n")
             for item in items:
                 level, offset = count(item, ">", True, True)
@@ -60,14 +58,15 @@ def md_wrap(text, width=70):
                 for line in textwrap.wrap(item[offset:], width - level - 1):
                     lines.append(">" * level + " " + line)
         elif paragraph.startswith("- ") or paragraph.startswith("-\t"):
+            # Unordered list - try to format it
             items = paragraph.split("\n")
             for item in items:
                 level, offset = count(item, "-", True, True)
                 for i, line in enumerate(textwrap.wrap(
                         item[offset:], width - level*2 - 1)):
                     lines.append("  " * (level-1) + ("  ", "- ")[i == 0] + line)
-                #lines.extend(textwrap.wrap(item, width))
         else:
+            # Paragraph
             lines.extend(textwrap.wrap(paragraph, width))
         lines.append("")
     return lines
@@ -90,9 +89,6 @@ def img2text(url, width):
     except subprocess.TimeoutExpired:
         p.kill()
         outs, errs = p.communicate()
-
-def bind(value, start, end):
-    return max(start, min(value, end))
 
 class Interface(object):
     def __init__(self, stdscr, data):
@@ -128,6 +124,10 @@ class Interface(object):
         self.main.refresh()
 
     def show(self):
+        """
+        Handles the list display window.  Drawing title and contents is
+        left up to `display_title` and `display_list`.
+        """
         cursor = 0
         while True:
             data = self.get_data()
@@ -149,7 +149,12 @@ class Interface(object):
             elif k == "KEY_NPAGE":
                 cursor = bind(cursor+(self.Y-3), 0, len(data)-1)
             elif k in ("KEY_ENTER", "\n", "\r"):
+                # Opens the entry under `cursor`, which could return
+                # a positive or negative integer if the user moves
+                # through entries while there.
                 move = self.show_entry(cursor)
+                # If a non-None value has been returned, move the
+                # cursor and display the appropriate entry.
                 while move is not None:
                     cursor = bind(cursor + move, 0, len(data)-1)
                     move = self.show_entry(cursor)
@@ -170,6 +175,7 @@ class Interface(object):
                     if k in ("KEY_ENTER", "\n", "\r"):
                         break
                     elif k in ("\x1b",):
+                        # Escape - clear the filter and quit
                         self.filter = None
                         break
                     elif k in ("KEY_BACKSPACE", "KEY_DC"):
@@ -183,6 +189,9 @@ class Interface(object):
                         break
 
     def display_title(self, left="q:quit h:help", right=""):
+        """
+        Draws the title window.
+        """
         self.title.clear()
         self.title.insstr(0, 0, "jsonl-viewer v.0.1.0 | " + left)
         if right:
@@ -190,6 +199,9 @@ class Interface(object):
         self.title.hline(1, 0, curses.ACS_HLINE, self.X)
 
     def display_list(self, cursor):
+        """
+        Draws the list view.
+        """
         self.main.clear()
         self.main.attrset(curses.color_pair(1))
         self.main.insstr(0, 0, " " * self.X)
@@ -203,7 +215,6 @@ class Interface(object):
             pos = 1 + i - start
             if i == cursor:
                 self.main.attrset(curses.color_pair(2))
-            # BUG: this errors somewhere around my 300th comment
             self.main.insstr(pos, 0, " " * self.X)
             title = item.get("title", item.get("link_title"))[:58]
             title = "".join(filter(lambda c: c in string.printable, title))
@@ -212,20 +223,21 @@ class Interface(object):
             self.main.attrset(0)
 
     def show_entry(self, cursor):
+        """
+        Handles the entry display window.
+        """
         data = self.get_data()
         entry = data[cursor]
         if "body" in entry:
             lines, line, kind = md_wrap(entry.get("body"), self.X-2), 0, "Comment"
         elif "selftext" in entry:
             lines, line, kind = md_wrap(entry["selftext"], self.X-2), 0, "Self-post"
-        #elif "thumbnail" in entry:
-        #    lines, line = img2text(entry.get("thumbnail"), self.X-2), 0
         else:
             lines, line, kind = [], 0, "Link"
         while True:
-            self.display_title("q:quit h:help o:browse", right="{}/{}".format(cursor+1, len(data)))
+            self.display_title("q:quit h:help o:browse", right="{}/{}".format(
+                cursor+1, len(data)))
             top = self.display_entry(entry)
-            #if "body" in entry:
             self.display_text(lines, line, top)
             self.refresh()
             k = self.stdscr.getkey()
@@ -242,11 +254,15 @@ class Interface(object):
             elif k in ("KEY_RIGHT", "l"):
                 return +1
             elif k == "o":
-                webbrowser.open_new_tab(entry.get("url", "https://reddit.com/" + entry.get("permalink")))
+                url = entry.get("url", "https://reddit.com/" + entry.get("permalink"))
+                webbrowser.open_new_tab(url)
             elif k == "q":
                 return
 
     def display_entry(self, entry):
+        """
+        Draws the contents of some entry.
+        """
         self.main.clear()
         title = entry.get("title", entry.get("link_title"))
         title = "".join(filter(lambda c: c in string.printable, title))
@@ -275,16 +291,16 @@ class Interface(object):
         return pos + 1
 
     def display_text(self, lines, line, top):
+        """
+        Draws some text in the main window.
+        """
         start, end = line, line + (self.Y-top-3)
         for i, line in enumerate(lines[start:end]):
             self.main.insstr(top + 1 + i, 1, line)
 
 if __name__ == "__main__":
     data = []
-    tail = []
     for line in fileinput.input():
         data.append(json.loads(line))
     locale.setlocale(locale.LC_ALL, "")
     curses.wrapper(Interface, data)
-    print("\n".join(tail))
-    

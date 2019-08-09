1c0da98d4af9a16676e7ae2c98b81d15b6a4ffcd
mftrhu <mftrhu+github@inventati.org>
2019-08-10 01:22:29 +0200

    Tweak yank-all and provide more examples in .viewerrc
    The output of yank-all.sh is now consistent with the format I have
    been using to store reddit comments in org, stashing more information
    about the comment away in the properties drawer.

--- yank-all.sh
+++ yank-all.sh
@@ -9,9 +9,14 @@ ctrl_c () {
   cat >"/tmp/$USER.clipboard"
 }
 {
-  printf "* /u/%s on [%s]\n" "$2" "$(date +"%Y-%m-%d %H:%M" --date="@$4")"
+  printf "* /u/%s\n" "$2"
   printf ":PROPERTIES:\n"
+  printf ":Author: %s\n" "$2"
+  printf ":Created: [%s]\n" "$(date +"%Y-%m-%d %H:%M" --date="@$4")"
   printf ":Permalink: %s\n" "$3"
+  printf ":Subreddit: %s\n" "$(echo "$3" | cut -d / -f 5)"
+  printf ":Saved: [%s]\n" "$(date +"%Y-%m-%d %H:%M")"
   printf ":END:\n\n"
   pandoc -f markdown -t org --wrap=none <<<"$5"
+  printf "\n"
 } | ctrl_c
